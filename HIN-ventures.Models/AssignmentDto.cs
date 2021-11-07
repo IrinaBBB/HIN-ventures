2b60d504e28f5a35bd06954bf817bbdff213c7cf
Irina <balkarovairina@gmail.com>
2021-11-07 15:48:48 +0100

    add: AssignmentDto

---
+++ HIN-ventures.Models/AssignmentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HIN_ventures.Models
+{
+    public class AssignmentDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Range(1, 3000)]
+        public double Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public DateTime Deadline { get; set; }
+    }
+}
