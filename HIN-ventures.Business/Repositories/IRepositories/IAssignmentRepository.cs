fc60beb59d07af1b88fde7633a4da744fd3d7cd3
Irina <balkarovairina@gmail.com>
2021-11-07 15:57:59 +0100

    add: IAssignmentRepository

---
+++ HIN-ventures.Business/Repositories/IRepositories/IAssignmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HIN_ventures.Models;
+
+namespace HIN_ventures.Business.Repositories.IRepositories
+{
+    public interface IAssignmentRepository
+    {
+        public Task<AssignmentDto> CreateAssignment(AssignmentDto assignmentDto);
+        public Task<AssignmentDto> UpdateAssignment(int assignmentId, AssignmentDto assignmentDto);
+        public Task<AssignmentDto> GetAssignment(int assignmentId);
+        public Task<IEnumerable<AssignmentDto>> GetAllAssignments();
+    }
+}
