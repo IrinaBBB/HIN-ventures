5cfd0d9a4dbba2e48a24d7ac1a613543c9248fd3
Irina <balkarovairina@gmail.com>
2021-11-07 13:19:43 +0100

    add: database access

---
+++ HIN-ventures.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HIN_ventures.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
