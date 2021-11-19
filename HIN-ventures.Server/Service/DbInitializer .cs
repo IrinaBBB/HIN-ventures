7490b9ebca3e4d02129628a83186bec7d45f97dc
torebjerkan <tbj034@post.uit.no>
2021-11-19 12:06:07 +0100

    Fix: Change IdentityUser to ApplicationUser in DbInitializer

--- HIN-ventures.Server/Service/DbInitializer .cs
+++ HIN-ventures.Server/Service/DbInitializer .cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using HIN_ventures.Common;
 using HIN_ventures.DataAccess.Data;
+using HIN_ventures.DataAccess.Entities;
 using HIN_ventures.Server.Service.IService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +14,10 @@ namespace HIN_ventures.Server.Service
     public class DbInitializer: IDbInitializer
     {
         private readonly ApplicationDbContext _db;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager,
+        public DbInitializer(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
             RoleManager<IdentityRole> roleManager)
         {
             _db = db;
@@ -44,14 +45,14 @@ namespace HIN_ventures.Server.Service
             _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
             _roleManager.CreateAsync(new IdentityRole(SD.Role_Freelancer)).GetAwaiter().GetResult();
 
-            _userManager.CreateAsync(new IdentityUser
+            _userManager.CreateAsync(new ApplicationUser()
             {
                 UserName = "admin@gmail.com",
                 Email = "admin@gmail.com",
                 EmailConfirmed = true
             }, "Admin123*").GetAwaiter().GetResult();
 
-            IdentityUser user = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
             _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
 
         }
