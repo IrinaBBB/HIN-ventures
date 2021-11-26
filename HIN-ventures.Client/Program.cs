3babb568c636ef2419f7dbc3b0cdc0565b889990
Irina <balkarovairina@gmail.com>
2021-11-26 19:57:00 +0100

    fix: errors after merge

--- HIN-ventures.Client/Program.cs
+++ HIN-ventures.Client/Program.cs
@@ -3,6 +3,12 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using HIN_ventures.Client.Service;
+using HIN_ventures.Client.Service.IService;
+using HIN_ventures_Client.Service;
+using HIN_ventures_Client.Service.IService;
+using Microsoft.Extensions.Configuration;
 
 namespace HIN_ventures.Client
 {
@@ -14,7 +20,6 @@ namespace HIN_ventures.Client
             builder.RootComponents.Add<App>("#app");
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = 
                 new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl")) });
             builder.Services.AddBlazoredLocalStorage();
