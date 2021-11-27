c48a9c47c0d30fda82330c543eab18bb83cd7751
torebjerkan <tbj034@post.uit.no>
2021-11-27 22:56:59 +0100

    add: wip customer client side

--- HIN-ventures.Client/Model/ViewModel/CustomerVM.cs
+++ HIN-ventures.Client/Model/ViewModel/CustomerVM.cs
@@ -5,12 +5,11 @@ using System.Threading.Tasks;
 
 namespace HIN_ventures_Client.Model.ViewModel
 {
-    public class HomeVM
+    public class CustomerVM
     {
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; }
         public int NoOfNights { get; set; } = 1;
-        public bool IsAvailable { get; set; } = true;
         public string Speciality { get; set; }
     }
 }
